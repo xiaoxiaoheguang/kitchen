248ce6898a8dbb7ef459c300c0e871f3b61292a3
xiaoxiaoheguang <xiaoxiaoheguang@163.com>
2026-03-23 00:08:58 +0800

    feat(Delivery):同步订单状态，更改控制台字体，导入中文字体

--- Assets/Script/Manager/DeliveryManager.cs
+++ Assets/Script/Manager/DeliveryManager.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class DeliveryManager : MonoBehaviour
+public class DeliveryManager : NetworkBehaviour
 {
     public static DeliveryManager Instance { get; private set; }
 
@@ -16,7 +17,7 @@ public class DeliveryManager : MonoBehaviour
     [SerializeField] private RecipeListSO recipeListSO;
     private List<RecipeSO> waitingRecipeSOList;
 
-    private float spawnRecipeTimer;
+    private float spawnRecipeTimer = 4f;
     private float spawnRecipeTimerMax = 4f;
 
     private int waitRecipeAmountMax = 4;
@@ -30,6 +31,9 @@ public class DeliveryManager : MonoBehaviour
 
     private void Update()
     {
+        if (!IsServer) return;
+
+
         spawnRecipeTimer -= Time.deltaTime;
         if (spawnRecipeTimer <= 0f)
         {
@@ -37,19 +41,27 @@ public class DeliveryManager : MonoBehaviour
 
             if (GameManager.Instance.IsGamePlaying() && waitingRecipeSOList.Count < waitRecipeAmountMax)
             {
-                RecipeSO waitRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
-                waitingRecipeSOList.Add(waitRecipeSO);
+                int waittingRecipeSOIndex = UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count);
 
-                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+                SpawnNewWaittingRecipeClientRpc(waittingRecipeSOIndex);
                 //Debug.Log(waitRecipeSO);
             }
         }
     }
 
+    [ClientRpc]
+    private void SpawnNewWaittingRecipeClientRpc(int waittingRecipeSOIndex)
+    {
+        RecipeSO waitRecipeSO = recipeListSO.recipeSOList[waittingRecipeSOIndex];
+        waitingRecipeSOList.Add(waitRecipeSO);
+
+        OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+    }
+
     public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
     {
         for (int i = 0; i < waitingRecipeSOList.Count; i++)
-        { 
+        {
             //遍历订单，查找递交的菜谱是否与 订单列表 里某项相同
             RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
 
@@ -81,23 +93,49 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe)
                 {
                     // Player delivered the correct recipe!
-                    Debug.Log("player deliver true recipe");
+                    //Debug.Log("player deliver true recipe");
 
-                    successAmount++;
-                    waitingRecipeSOList.RemoveAt(i);
-
-
-                    OnRecipeCompleted?.Invoke(this,EventArgs.Empty);
-                    OnRecipeSuccess?.Invoke(this,EventArgs.Empty);
+                    DeliverCorrectRecipeServerRpc(i);
                     return;
                 }
             }
         }
-        OnRecipeFailed?.Invoke(this,EventArgs.Empty);
+        DeliverIncorrectRecipeServerRpc();
         // No matches found!
         // Player did not deliver a correct recipe
 
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DeliverIncorrectRecipeServerRpc()
+    {
+        DeliverIncorrectRecipeClientRpc();
+    }
+
+    [ClientRpc]
+    private void DeliverIncorrectRecipeClientRpc()
+    {
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DeliverCorrectRecipeServerRpc(int waittingRecipeSOIndex)
+    {
+        DeliverCorrectRecipeClientRpc(waittingRecipeSOIndex);
+    }
+
+    [ClientRpc]
+    private void DeliverCorrectRecipeClientRpc(int waittingRecipeSOIndex)
+    {
+        successAmount++;
+
+        waitingRecipeSOList.RemoveAt(waittingRecipeSOIndex);
+
+        OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+        OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
+    }
+
+
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
